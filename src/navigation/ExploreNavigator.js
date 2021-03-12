5fccb8a49f323f0e7c8cd10d2d1d7b84af8340c4
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2021-03-13 00:49:56 +0800

    add top tab bars

--- src/navigation/ExploreNavigator.js
+++ src/navigation/ExploreNavigator.js
@@ -2,20 +2,22 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import HomeScreen from '../screens/Home';
-import SearchResultsScreen from '../screens/SearchResults';
 
+import SearchResultsTabNavigator from './SearchResultsTabNavigator';
 const Stack = createStackNavigator();
 const Router = () => {
   return (
     <Stack.Navigator>
+      {/* 最先出現 default page*/}
       <Stack.Screen
         name={'Welcome'}
         component={HomeScreen}
         options={{headerShown: false}}
       />
+      {/* 因為是 stack 所以覆蓋上頭的 screen */}
       <Stack.Screen
         name={'SearchResults'}
-        component={SearchResultsScreen}
+        component={SearchResultsTabNavigator}
         options={{title: '搜尋地點'}}
       />
     </Stack.Navigator>
