2bd34bc1a84923e96fd8781832bd52914dc0da3f
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2021-03-13 10:33:04 +0800

    auto place complate done

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,34 +1,34 @@
 import React, {useState} from 'react';
 import {View, Text, TextInput, FlatList, Pressable} from 'react-native';
 import styles from './styles';
-import searchResults from '../../../assets/data/search';
-import Entypo from 'react-native-vector-icons/Entypo';
+// import searchResults from '../../../assets/data/search';
+
 import {useNavigation} from '@react-navigation/native'; //轉址用
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import SuggestionRow from './SuggestionRow';
 const DestinationSearchScreen = () => {
   const [inputText, setInputText] = useState('');
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       {/* input */}
-      <TextInput
+      <GooglePlacesAutocomplete
         style={styles.textInput}
         placeholder="想去哪裡?"
-        value={inputText}
-        onChangeText={setInputText}
-      />
-      {/* list of destination */}
-      <FlatList
-        data={searchResults}
-        renderItem={({item}) => (
-          <Pressable
-            style={styles.row}
-            onPress={() => navigation.navigate('Guests')}>
-            <View style={styles.iconContainer}>
-              <Entypo name="location-pin" size={30} />
-            </View>
-            <Text style={styles.locationText}>{item.description}</Text>
-          </Pressable>
-        )}
+        renderRow={(item) => <SuggestionRow item={item} />} // 客製自己的 result row
+        suppressDefaultStyles // 取消預設style
+        enablePoweredByContainer={false} // 取消底下 Power by Google 浮水印
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          navigation.navigate('Guest');
+        }}
+        fetchDetails // 取得地點細節，例如經緯度ㄋ
+        query={{
+          key: 'AIzaSyAd6ViOoUVY6q5cacsG54kiTLLTmWpRoqU',
+          language: 'zh-TW',
+          types: '(cities)', // return only cities
+        }}
       />
     </View>
   );
--- src/screens/DestinationSearch/styles.js
+++ src/screens/DestinationSearch/styles.js
@@ -2,7 +2,9 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
+    height: '100%',
+    backgroundColor: 'white',
+    padding: 20,
   },
   textInput: {
     fontSize: 16,
